0595d7ecd73ef892ccdbce3af3c13d5d117fda24
Carlos <carloscz25@gmail.com>
2022-02-04 15:39:41 +0100

    07012022

--- modules/web/src/com/company/test1/web/screens/NoEmitidosOAnomalos.java
+++ modules/web/src/com/company/test1/web/screens/NoEmitidosOAnomalos.java
@@ -1,10 +1,68 @@
 package com.company.test1.web.screens;
 
-import com.haulmont.cuba.gui.screen.Screen;
-import com.haulmont.cuba.gui.screen.UiController;
-import com.haulmont.cuba.gui.screen.UiDescriptor;
+import com.company.test1.entity.ItemNoEmitidoOAnomalo;
+import com.company.test1.entity.departamentos.Departamento;
+import com.company.test1.service.NoEmitidosOAnomalosService;
+import com.haulmont.cuba.core.global.LoadContext;
+import com.haulmont.cuba.gui.Notifications;
+import com.haulmont.cuba.gui.components.Button;
+import com.haulmont.cuba.gui.components.DateField;
+import com.haulmont.cuba.gui.model.CollectionLoader;
+import com.haulmont.cuba.gui.screen.*;
+
+import javax.inject.Inject;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 @UiController("test1_NoEmitidosOAnomalos")
 @UiDescriptor("no-emitidos-o-anomalos.xml")
 public class NoEmitidosOAnomalos extends Screen {
+    @Inject
+    private DateField<Date> fechaDesdeField;
+    @Inject
+    private DateField<Date> fechaHastaField;
+    @Inject
+    private CollectionLoader<ItemNoEmitidoOAnomalo> itemsDl;
+    @Inject
+    private NoEmitidosOAnomalosService noEmitidosOAnomalosService;
+    @Inject
+    private Notifications notifications;
+
+    @Subscribe("cargar")
+    public void onCargarClick(Button.ClickEvent event) {
+        itemsDl.load();
+
+
+    }
+
+    @Install(to = "itemsDl", target = Target.DATA_LOADER)
+    private List<ItemNoEmitidoOAnomalo> itemsDlLoadDelegate(LoadContext<ItemNoEmitidoOAnomalo> loadContext) {
+        List<ItemNoEmitidoOAnomalo> l = new ArrayList();
+        try{
+            Date fechaDesde = fechaDesdeField.getValue();
+            Date fechaHasta = fechaHastaField.getValue();
+
+            List<Departamento> dd = noEmitidosOAnomalosService.getDepartamentosMonitorizados();
+            for (int i = 0; i < dd.size(); i++) {
+                Object[] oo = noEmitidosOAnomalosService.reportarDepartamento(dd.get(i), fechaDesde, fechaHasta);
+                if ((boolean)oo[0]){
+                    ItemNoEmitidoOAnomalo item = new ItemNoEmitidoOAnomalo();
+                    item.setDepartamento(dd.get(i));
+                    item.setInformacionReportada((String)oo[1]);
+                    l.add(item);
+                }
+
+
+            }
+            return l;
+        }catch(Exception exc){
+            notifications.create().withDescription(exc.getMessage()).show();
+        }
+    }
+
+
+
+
+
 }
